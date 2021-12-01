2285ae699b6446f03cfb418ca0685319a389b9b0
juancacorps <juancacorps@gmail.com>
2021-11-30 23:50:08 -0600

    remove fields

---
+++ pos_product_information/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+	'name' : 'Product Information in POS',
+	'author': "Juan Carlos Rojas",
+	'version' : '13.0.1.0',
+	'summary' : 'Product Information in POS',
+	'description' : 'Product Information in POS',
+	'depends' : ['point_of_sale'],
+	'data' : [],
+	'qweb' : ['static/src/xml/pos_product_info.xml'],
+	'demo' : [],
+    'application': True,
+    'sequence': 1,
+	'installable' : True,
+	'auto_install' : False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
